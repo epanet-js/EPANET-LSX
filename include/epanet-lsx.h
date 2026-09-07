48a513a86590852bd610d7eb09bb040527bdb1f2
Cesar Gonzalez <cesar@iterating.ca>
2026-09-07 13:12:04 +0200

    Increase max iterations until convergence to 10

--- include/epanet-lsx.h
+++ include/epanet-lsx.h
@@ -1,7 +1,7 @@
 #pragma once
 #include "epanet2_2.h"
 
-#define MAX_LSX_ITERATIONS 5
+#define MAX_LSX_ITERATIONS 10
 
 int LSX_open(EN_Project project, const char *inp_path);
 int LSX_init(EN_Project project);
